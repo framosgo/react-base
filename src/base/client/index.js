ea84be2575604eccfb8e97bb619498f314a98e0c
Fernando Ramos <ferramgon1@gmail.com>
2018-12-20 18:12:37 +0100

    decouples routing

--- src/base/client/index.js
+++ src/base/client/index.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+import { BrowserRouter } from 'react-router-dom'
 
 import InitialState from 'store/InitialState'
 import ConfigureStore from 'store/ConfigureStore'
 
-import App from 'containers/App/'
-import Calculator from 'containers/Calculator/'
-import Main from 'containers/Main/'
-import WeatherStations from 'containers/WeatherStations/'
+import Routes from '../Routes'
 
 const history = createBrowserHistory()
 
@@ -19,11 +16,7 @@ const store = ConfigureStore(history, InitialState)
 render(
   <Provider store={store}>
     <BrowserRouter basename='/'>
-      <App>
-        <Route exact path='/' component={Main} />
-        <Route path='/calculator' component={Calculator} />
-        <Route path='/weatherstations' component={WeatherStations} />
-      </App>
+      <Routes />
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
---
+++ src/base/routes/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Calculator from 'containers/Calculator/'
+import Main from 'containers/Main/'
+import WeatherStations from 'containers/WeatherStations/'
+
+const Routes = () => (
+  <>
+    <Route exact path='/' component={Main} />
+    <Route path='/calculator' component={Calculator} />
+    <Route path='/weatherstations' component={WeatherStations} />
+  </>
+)
+
+export default Routes
